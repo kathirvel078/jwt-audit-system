407398a022197fea139af72892d829d7fdaee345
kathirvel078 <kathirvelvlr108@gmai.com>
2026-04-21 11:08:58 +0530

    Fix bycrypt.js

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt.js");
 const User = require("./models/User");
 
 
